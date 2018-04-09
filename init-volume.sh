0c8e5bd3b5f7f7f9b7a44657be18a5a9ea195c4a
Teruo OSHIDA <teruo.oshida@monstar-lab.com>
2018-04-09 11:02:22 +0900

    change method to run zbx-db container

--- init-volume.sh
+++ init-volume.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 docker-compose run --rm --user root zbx-db /root/prepare-varlib.sh
-docker-compose run -d zbx-db
+
+# docker-compose run -d zbx-db
+docker-compose up -d
 docker-compose exec --user root zbx-db /root/create-zbxdb.sh
 docker-compose down
