b4abf80c1a092dfa0097712494b1343b34768a64
Teruo OSHIDA <teruo.oshida@monstar-lab.com>
2018-05-24 13:32:46 +0900

    modify to wait until mysqld is ready

--- init-volume.sh
+++ init-volume.sh
@@ -3,6 +3,12 @@
 # create db
 
 docker-compose up -d
+while :
+do
+  STDOUT=$(docker-compose exec --user root zbx-db mysqladmin --no-defaults -uroot -proot ping 2>/dev/null)
+  echo "$STDOUT" | grep -q "mysqld is alive" && break
+  sleep 1
+done
 docker-compose exec --user root zbx-db mysql -uroot -proot -e "create database zabbix default character set utf8;"
 docker-compose exec --user root zbx-db mysql -uroot -proot -e "grant all privileges on zabbix.* to zabbix@'%' identified by 'zabbix';"
 docker-compose down
