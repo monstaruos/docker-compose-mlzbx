8555a4df352b763a21b5bac41bb96627ae04729c
Teruo OSHIDA <teruo.oshida@monstar-lab.com>
2018-05-24 10:58:54 +0900

    terminate local mariadb container then switch to official mysql:5.7

--- build-all.sh
+++ build-all.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-for x in mariadb; do
-  cd $x
-  docker build -t $x .
-  cd ..
-done
-
 for x in base server agent frontend; do
   cd $x
   docker build -t zbx-$x .
--- init-volume.sh
+++ init-volume.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-docker-compose run --rm --user root -v $(pwd)/server:/root/host zbx-db     /root/host/create-zbxdb.sh
+# create db
+
+docker-compose up -d
+docker-compose exec --user root zbx-db mysql -uroot -proot -e "create database zabbix default character set utf8;"
+docker-compose exec --user root zbx-db mysql -uroot -proot -e "grant all privileges on zabbix.* to zabbix@'%' identified by 'zabbix';"
+docker-compose down
+
+
+# initialize db
+
 docker-compose run --rm --user root -v $(pwd)/server:/root/host zbx-server /root/host/init-zbxdb.sh
+docker-compose down
